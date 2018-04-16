63cd94338b9c32eb8515f0b35dcb0fa8a8088887
Zane <contact@zane.sh>
2018-04-15 21:01:16 -0700

    Only redirect if it is a video

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let url = new URL(window.location.href);
 let old_domain = 'youtube.com';
 let new_domain = 'hooktube.com';
 
-if(url.hostname.indexOf(old_domain)) {
+if(url.hostname.indexOf(old_domain) >= 0 && url.pathname === "/watch") {
   let new_url = url.href.replace(old_domain, new_domain);
   window.location.href = new_url;
 }
